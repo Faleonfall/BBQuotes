4ca4e77c3b36dd5fdf480200ee4175c499c62877
Volodymyr Kryvytskyi <faleonfall@gmail.com>
2024-11-24 13:35:54 +0100

    Added CharacterView

--- BBQuotes/QuoteView.swift
+++ BBQuotes/QuoteView.swift
@@ -11,6 +11,8 @@ struct QuoteView: View {
     let vm = ViewModel()
     let show: String
     
+    @State var showCharacterInfo = false
+    
     var body: some View {
         GeometryReader { geo in
             ZStack {
@@ -21,12 +23,14 @@ struct QuoteView: View {
                 VStack {
                     VStack {
                         Spacer(minLength: 60)
-                        
                         switch vm.status {
+                            
                         case .notStarted:
                             EmptyView()
+                            
                         case .fetching:
                             ProgressView()
+                            
                         case .sucsess:
                             Text("\"\(vm.quote.quote)\"")
                                 .minimumScaleFactor(0.5)
@@ -55,6 +59,10 @@ struct QuoteView: View {
                             }
                             .frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
                             .clipShape(.rect(cornerRadius: 50))
+                            .onTapGesture {
+                                showCharacterInfo.toggle()
+                            }
+                            
                         case .failed(let error):
                             Text(error.localizedDescription)
                         }
@@ -85,6 +93,9 @@ struct QuoteView: View {
             .frame(width: geo.size.width, height: geo.size.height)
         }
         .ignoresSafeArea()
+        .sheet(isPresented: $showCharacterInfo) {
+            CharacterView(character: vm.character, show: show)
+        }
     }
 }
 
